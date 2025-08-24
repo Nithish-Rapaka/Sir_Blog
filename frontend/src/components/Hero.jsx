347e2a6c164216a4cef3f9a28285dc17a7654eac
Nithish-Rapaka <rapakanithish83@gmail.com>
2025-08-24 10:06:14 +0530

    Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
             
             {/* Heading */}
             <h1 className="text-5xl sm:text-6xl font-bold leading-tight mb-6 tracking-tight font-serif">
-              Especially Designed for IT Minds
-            </h1>
+              
+            </h1>Drmalleswararao's Blog
             
             <p className="mb-8 text-lg leading-relaxed text-gray-600 max-w-xl font-light">
               The journey of a student is filled with challenges and triumphs. 
